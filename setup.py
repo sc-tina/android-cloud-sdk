c1341aaa2a6ed656e074799ec07798ae9dfc8d6e
sc-tina <nebulae-86yarns@icloud.com>
2026-07-15 19:17:19 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+setup(name='android-cloud-sdk', version='0.1.0',
+      description='Python SDK for managing cloud Android devices via ADB',
+      url='https://github.com/sc-tina/android-cloud-sdk',
+      packages=find_packages(), python_requires='>=3.8')
